fecb942b92e7dd5dfb7cd699a8bf86822fc01a5a
ceciisalas <cecizs1@hotmail.com>
2022-07-06 20:47:52 +0000

    c practice

---
+++ 2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlih.h>
+/*
+ * main: Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a, z;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar (a);
+	}
+	putchar('\n');
+	return (0);
+}
