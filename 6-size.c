ab935cb845ace04f6baa39b4248281706e692910
ceciisalas <cecizs1@hotmail.com>
2022-07-06 17:20:45 +0000

    c practice

---
+++ 6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main: entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int integerType;
+	char charType;
+	float floatType;
+	double doubleType;
+
+	printf("Size of int is: %ld\n", sizeof(integerType));
+	printf("Size of char is: %ld\n", sizeof(charType));
+	printf("Size of float is: %ld\n", sizeof(floatType));
+	printf("Size of double is: %ld\n". sizeof(doubleType));
+
+	return (0);
+}
